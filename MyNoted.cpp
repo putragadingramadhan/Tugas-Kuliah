8745e6c1113601f3032f5a106deb70e0e4021fc9
Putra Gading Ramadhan <putragading13618@gmail.com>
2026-03-04 16:22:37 +0700

    Add files via upload

--- MyNoted.cpp
+++ MyNoted.cpp
@@ -6,7 +6,7 @@ isinya berupa file logika tentang cout (untuk print) dan cin (untuk input)
 using namespace std; */
 
 #include <string>// tipe data string bukan bawaan asli c++, makanya perlu kita panggil dulu
-
+#include <initializer_list>
 // penggunaan struct, untuk membuat tipe data baru
 struct Mahasiswa {
     std :: string nama;
@@ -21,15 +21,30 @@ class Mobil {
     std :: string warna;
 };
 
+//fnction tidak membalikan nilai
 void nama(std :: string nama, int umur){
     std :: cout << "Hallo "<< nama << ", Selamat masuk umur " << umur <<std :: endl;
 }
+
+//function membalikkan nilai
 int luas_pesergiPNjng (int tinggi, int lebar){
     int luas = tinggi*lebar;
     return luas;
 }
+int pertambahan_array (int daftar_angka [], int n){
+    int total = 0;
+    for (int i=0; i<n;i++){
+        total+=daftar_angka[i];
+    }
+    return total;
+}
 
+typedef int angka; /*Tydef berfungsi seperti alisan,
+analoginya seperti kita aku biasa dipanggil 'Gading' tapi kadang aku pengen di panggil 'abang'
+nah typedef memeungkinkan kita mengganti deklarasi variabel tanpa menggunakan keyword seperti int, float, dll
+fungsinya adalah agar lebih mudah dimengerti dan dibaca manusia*/
 int main (){
+    angka nilai = 100; // dia otomatis sebagai tipe data int
     //variabel hanya dapat menyinpan 1 nilai, contoh
     // int x = 30,40;//akan error
     int x = 40;
@@ -42,7 +57,7 @@ int main (){
     Tipe data bilangan bulat
     ======*/
     int jumlah_mahasiswa = 900;
-    short nilai = 89;
+    short Nilai = 89;
     long data_followers = 1000000;
     long long data_penduduk = 2000000000;
     
@@ -57,8 +72,8 @@ int main (){
     ======
     Tipe data teks
     ======*/
-    char predikat_nilai = 'A';
-    std :: string status = "Lulus";
+    char predikat_nilai = 'A'; //  character
+    std :: string status = "Lulus"; // string
 
     if(nilai > 85 && predikat_nilai == 'A'){
         std :: cout << status << std :: endl;
@@ -66,6 +81,10 @@ int main (){
         std :: cout << "Anda belum lulus" << std:: endl;
     }
 
+    /*
+    ======
+    Tipe data boolean
+    ======*/
     bool udah_nikah = true;
     bool belum_nikah = false;
     bool dan = udah_nikah && belum_nikah;
@@ -111,6 +130,107 @@ int main (){
     std :: cin >> lebar;
     std :: cout << luas_pesergiPNjng(tinggi,lebar)<< "m2"<< std :: endl;
 
+    /*
+    ======
+    Panggil function pertambahan_array
+    ======*/
+    int angka [] ={30,80,90,60,70};
+    int length = sizeof(angka)/sizeof(angka[0]);
+    std :: cout << "\nHasil pertambahan array : " << pertambahan_array(angka,length) << std :: endl;
+    
+    
+    /*
+    ======
+    Operstor Aritmatika
+    ======*/
+    int a = 10, b = 3; 
+    
+    std :: cout << "\nPenjumlahan: " << a + b << std ::endl;
+    std :: cout << "Pengurangan: " << a - b << std :: endl;
+    std :: cout << "Perkalian: " << a * b << std :: endl;
+    std :: cout << "Pembagian: " << a / b << std :: endl; 
+    std :: cout << "Modulus: " << a % b << std :: endl; 
+    
+    /*
+    ======
+    Operstor relasi (perbandingan)
+    ======*/
+    int A, B; 
+    
+    std :: cout << "\nMasukkan angka pertama: "; std :: cin >> A;
+    std :: cout << "Masukkan angka kedua: "; std :: cin >> B; 
+    
+    std :: cout << "a == b : " << (A == B) << std :: endl;  // Sama dengan     
+    std :: cout << "A != B : " << (A != B) << std ::endl;  // Tidak sama dengan     
+    std ::cout << "A > B  : " << (A > B) << std :: endl;   // Lebih besar     
+    std :: cout << "A < B  : " << (A < B) << std :: endl;   // Lebih kecil     
+    std:: cout << "A >= B : " << (A >= B) << std :: endl;  // Lebih besar sama dengan     
+    std ::cout << "A <= B : " << (a <= B) << std::endl;  // Lebih kecil sama dengan 
+    
+    /*
+    ======
+    Operstor logika
+    ======*/
+    int UMUR; 
+    bool punyaKartu = false; 
+
+    std :: cout << "Masukkan UMUR Anda: "; std :: cin >> UMUR; 
+
+    // Contoh operator AND (&&)     
+    if (UMUR >= 18 && UMUR <= 60) { 
+        std :: cout << "Usia Anda termasuk dewasa." << std ::endl; 
+    } else { 
+        std ::cout << "Usia Anda tidak termasuk dewasa." <<std :: endl; 
+    } 
+
+    // Contoh operator OR (||)     
+    if (UMUR < 18 || UMUR > 60) {         
+        std ::cout << "Anda harus didampingi orang dewasa." << std ::endl; 
+    } else { 
+        std ::cout << "Anda boleh masuk sendiri." <<std :: endl; 
+    } 
+
+    // Contoh operator NOT (!)     
+    if (!punyaKartu) {        
+        std ::cout << "Anda belum memiliki kartu." << std ::endl; 
+    } else { 
+        std ::cout << "Anda sudah memiliki kartu." << std ::endl; 
+    } 
+
+    /*
+    ======
+    Operator penugasan
+    ======*/
+
+    int z = 10; 
+    z += 5;   // z = z + 5 
+    std :: cout << "z += 5  : " << z << std ::endl; 
+
+    z -= 3;   // z = z - 3 
+    std ::cout << "z -= 3  : " << z << std ::endl; 
+
+    z *= 2;   // z = z * 2 
+    std ::cout << "z *= 2  : " << z <<std :: endl; 
+
+    z /= 4;   // z = z / 4 
+    std ::cout << "z /= 4  : " << z << std ::endl; 
+
+    /*
+    ======
+    Operator increment dan decrement
+    ======*/
+    int bilangan = 5; 
+    bilangan++;   // Increment (tambah 1)     
+    std ::cout << "Setelah increment: " << bilangan <<std :: endl; 
+
+    bilangan--;   // Decrement (kurang 1)     
+    std ::cout << "Setelah decrement: " << bilangan <<std :: endl; 
+
+
+
+
+
+
 
     /*Jadi, aku mau coba membuat program sederhana tentang membeli buku
     Algoritmanya
@@ -120,9 +240,13 @@ int main (){
     4. jika harga buku >120.000, maka dapat diskon 10%
     5. user ditampilkan bill nya
     6. selesai*/
+    /*
+    ======
+    Tipe data array
+    ======*/
     std :: string  buku [3]={"Psychology of money", "Rich Dad Poor Dad", "Start With Why"}; //nama buku
-    int harga_buku [] ={50000,70000,30000}; // harga
-    std :: cout << "==Welcome to Gragading Store==" << std :: endl;
+    int harga_buku [3] ={50000,70000,30000}; // harga
+    std :: cout << "\n==Welcome to Gragading Store==" << std :: endl;
     std :: cout << "==" << std :: endl;
     int pilihan, qty;
     int len = sizeof(harga_buku)/sizeof(harga_buku[0]); //untuk mengecek stok buku berdasarkan index
@@ -156,6 +280,8 @@ int main (){
         int dikurang = (total_harga * 10)/100;
         diskon = dikurang;
         total_bayar_DGNDiskon = total_harga - dikurang;
+    } else if (total_harga <= 120000){
+        total_bayar_DGNDiskon = total_harga;
     }
     // tampilkan ke user
     std :: cout << "\n--- Detail Pembelian ---" << std :: endl;
