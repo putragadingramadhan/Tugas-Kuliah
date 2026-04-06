fc24ca576f6ede17e8a51c821811089bb22f033f
Putra Gading Ramadhan <putragading13618@gmail.com>
2026-04-06 14:03:46 +0700

    Add files via upload

--- TugasStack.cpp
+++ TugasStack.cpp
@@ -34,24 +34,23 @@ void display() {
         cout << "\nStack sekarang kosong." << endl;
     } else {
         cout << "\nIsi Stack saat ini (dari atas ke bawah):" << endl;
-        for (int i = maksimal-1; i >=0; i--) { // ambil datanya dari index terakhir (Fist in Last Out)
-            cout << "Data index [" << i << "] : " << panjang_jalan[i] << endl;
+        for (int i = top-1; i >=0; i--) { // ambil datanya dari index terakhir (Fist in Last Out)
+            cout << "Data index [" << i+1 << "] : " << panjang_jalan[i] << endl;
         }
     }
 }
+
 void input() {
     int s;
-    int counter = 1; // Agar user tahu ini input data ke-berapa
 
-    while (true) {
-        cout << "\nInput ke-" << counter << endl;
+    for(int max = 1;max<=10;max++){
+        cout << "\nInput ke-" << max << endl;
         cout << "Masukkan panjang jalan: ";
         cin >> s;
 
-        
         // selesai jika user input nilai 999, dan  999 tidak akan di simpan ke stack
         if (s == 999) {
-            cout << "Input dihentikan (999)." << endl;
+            cout<<"Input terdeteksi 999.\n" << "Input dihentikan." << endl;
             break; 
         }
 
@@ -63,7 +62,6 @@ void input() {
             } else {
                 push(s); //tambahka data ke stack
                 cout << "Data " << s << " berhasil disimpan ke Stack." << endl;
-                counter++; // tambah nomor urut input hanya jika berhasil simpan
             }
         } 
         // inputan <60, keluarkan isi data dan berhentika program
@@ -77,17 +75,34 @@ void input() {
                 cout<<"Data sementara ditampilkan"<<endl;
                 // push(s);
                 display();
-                // counter ++;
-                continue;
+                max--; // tambah nomor urut input hanya jika berhasil simpan
+                // max ++;
+                // continue;
                 // break;
             }
-            
-
         }
+    } 
+}
+
+void kondisi_stack(){
+    cout<<"\n===KONDISI STACK==="<<endl;
+    if(isEmpty()){
+            cout<<"kosong"<<endl;
     }
+    if(isFull()){
+        cout<<"penuh"<<endl;
+    }else{
+        cout<<"bisa diisi (tidak penuh)"<<endl;
+    }
+    if(!isEmpty()){
+        cout<<"ada isiya"<<endl;
+        cout<<"jumlah data stack saat ini "<< top <<endl;
+    }
+
 }
 int main() {
     input();
     display();
+    kondisi_stack();
     return 0;
 }
